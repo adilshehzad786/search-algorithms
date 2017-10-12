fd5853b0e3ca17cefc18584e2c552b61d0444657
Victor Osorio <victor.perticarrari@gmail.com>
2017-10-12 10:11:28 -0300

    Initial Version

--- driver.py
+++ driver.py
@@ -8,3 +8,14 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         eprint("usage: python driver.py [method] [board]")
         sys.exit(1)
+
+    board = []
+    row = []
+    actualIndex = 0
+    for item in sys.argv[2].split(','):
+        if actualIndex % 3 == 0:
+            row = []
+            board.append(row)
+        row.append(item)
+        actualIndex += 1
+    print(board, sys.argv[2].split(','))
